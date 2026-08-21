ff2efd788a3d741bb8243eb747b903b3ab8f80ac
rdwzfcckn5-hash <rdwzfcckn5@privaterelay.appleid.com>
2026-08-22 00:53:47 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.clients.claim();})()));self.addEventListener('fetch',()=>{});
